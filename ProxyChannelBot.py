46a13b2724cb88ab0da6ac7ae433dd3db62dae8b
Tho Tho <143883196+uwuB0y@users.noreply.github.com>
2023-09-04 12:36:17 +0300

    ProxyChannelBot.py

--- ProxyChannelBot.py
+++ ProxyChannelBot.py
@@ -1,8 +1,9 @@
 import telebot, time, requests
+chatid = "@urchanneluser"
 while True: 
     try:
-        rq=requests.get("https://gimmeproxy.com/api/getProxy")
-        telebot.TeleBot("TOKEN").send_message(chatid,f"*- New Proxy*\n*Ip-Port:* `{rq.json()["ipPort"]}`.\n*Type:* {rq.json()["protocol"]}.\n*Speed:* {rq.json()["speed"]}\n*Country:* {rq.json()["country"]}\n----- ------- ------- ----\n=> [archlinux](t.me/linux_nerd)",parse_mode="markdown",disable_web_page_preview=True)
+        rq = requests.get("https://gimmeproxy.com/api/getProxy")
+        telebot.TeleBot("TOKEN").send_message(chatid,f"*- New Proxy*\n*Ip-Port:* `{rq.json()["ipPort"]}`.\n*Type:* {rq.json()["protocol"]}.\n*Speed:* {rq.json()["speed"]}\n*Country:* {rq.json()["country"]}\n----- ------- ------- ----\n-=> [404](t.me/teamon404)",parse_mode="markdown",disable_web_page_preview=True)
         time.sleep(10)
     except:
         pass
