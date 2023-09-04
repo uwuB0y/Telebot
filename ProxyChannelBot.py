4f3971b1eb476a3dab5ec7211e9f90b29e2016e1
Tho Tho <143883196+uwuB0y@users.noreply.github.com>
2023-09-04 11:47:33 +0300

    ProxyChannelBot.py

--- ProxyChannelBot.py
+++ ProxyChannelBot.py
@@ -1,8 +1,8 @@
 import telebot, time, requests, os
 
 token = "TOKEN"  # bot token
-chatid = "@n30arch" # channel username or your id
-name= "archlinux" # The name to be displayed as the link to your channel
+chatid = "@urUserChannel"
+chatidd = "userChannel without @"
 delay = 10 # Delay between each message
 bot = telebot.TeleBot(token)
 while True: 
@@ -12,7 +12,7 @@ while True:
         ipport = rq.json()["ipPort"]
         speed = rq.json()["speed"]
         country = rq.json()["country"]
-        messageProxy = f"*- New Proxy*\n*Ip-Port:* `{ipport}`.\n*Type:* {protocol}.\n*Speed:* {speed}\n*Country:* {country}----- ------- ------- ----\n=> [{name}](t.me/{chatid})"
+        messageProxy = f"*- New Proxy*\n*Ip-Port:* `{ipport}`.\n*Type:* {protocol}.\n*Speed:* {speed}\n*Country:* {country}\n----- ------- ------- ----\n=> [Follow us](t.me/{chatidd})"
         bot.send_message(chatid, messageProxy, parse_mode="markdown", disable_web_page_preview=True) # Send the message
         time.sleep(delay) # Delay
     except Exception as e:
