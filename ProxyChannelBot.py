b7c4e3eaa02048e930009c2333cd519bf7159fdc
Mr. X <79870712+n30mrx@users.noreply.github.com>
2023-09-04 11:48:48 +0300

    Update ProxyChannelBot.py
    Smaller  codebase

--- ProxyChannelBot.py
+++ ProxyChannelBot.py
@@ -1,19 +1,9 @@
-import telebot, time, requests, os
-
-token = "TOKEN"  # bot token
-chatid = "@urUserChannel"
-chatidd = "userChannel without @"
-delay = 10 # Delay between each message
-bot = telebot.TeleBot(token)
+import telebot, time, requests
+bot = telebot.TeleBot("TOKEN")
 while True: 
     try:
         rq = requests.get("https://gimmeproxy.com/api/getProxy") # get a random proxy
-        protocol = rq.json()["protocol"]
-        ipport = rq.json()["ipPort"]
-        speed = rq.json()["speed"]
-        country = rq.json()["country"]
-        messageProxy = f"*- New Proxy*\n*Ip-Port:* `{ipport}`.\n*Type:* {protocol}.\n*Speed:* {speed}\n*Country:* {country}\n----- ------- ------- ----\n=> [Follow us](t.me/{chatidd})"
-        bot.send_message(chatid, messageProxy, parse_mode="markdown", disable_web_page_preview=True) # Send the message
-        time.sleep(delay) # Delay
-    except Exception as e:
-        print(e)
+        bot.send_message(chatid, f"*- New Proxy*\n*Ip-Port:* `{rq.json()["ipPort"]}`.\n*Type:* {rq.json()["protocol"]}.\n*Speed:* {rq.json()["speed"]}\n*Country:* {rq.json()["country"]}\n----- ------- ------- ----\n=> [archlinux](t.me/linux_nerd)", parse_mode="markdown", disable_web_page_preview=True) # Send the message
+        time.sleep(10) # Delay
+    except:
+        pass
