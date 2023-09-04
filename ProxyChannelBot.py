eef817777852bd1eba0caa25bb5d58fb6ea4b50e
Mr. X <79870712+n30mrx@users.noreply.github.com>
2023-09-04 11:57:47 +0300

    Update ProxyChannelBot.py
    NOW IT IS LITERALLY JUST 8 FUCKING LINES

--- ProxyChannelBot.py
+++ ProxyChannelBot.py
@@ -1,9 +1,8 @@
 import telebot, time, requests
-bot = telebot.TeleBot("TOKEN")
 while True: 
     try:
-        rq = requests.get("https://gimmeproxy.com/api/getProxy") # get a random proxy
-        bot.send_message(chatid, f"*- New Proxy*\n*Ip-Port:* `{rq.json()["ipPort"]}`.\n*Type:* {rq.json()["protocol"]}.\n*Speed:* {rq.json()["speed"]}\n*Country:* {rq.json()["country"]}\n----- ------- ------- ----\n=> [archlinux](t.me/linux_nerd)", parse_mode="markdown", disable_web_page_preview=True) # Send the message
-        time.sleep(10) # Delay
+        rq=requests.get("https://gimmeproxy.com/api/getProxy")
+        telebot.TeleBot("TOKEN").send_message(chatid,f"*- New Proxy*\n*Ip-Port:* `{rq.json()["ipPort"]}`.\n*Type:* {rq.json()["protocol"]}.\n*Speed:* {rq.json()["speed"]}\n*Country:* {rq.json()["country"]}\n----- ------- ------- ----\n=> [archlinux](t.me/linux_nerd)",parse_mode="markdown",disable_web_page_preview=True)
+        time.sleep(10)
     except:
         pass
